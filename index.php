12d0908fa7cb3ef4ff8ca930840554779aca5d64
Uduak Essien <acushla4real@gmail.com>
2019-09-19 19:49:52 +0100

    css update for mobile view

--- index.php
+++ index.php
@@ -77,10 +77,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
+            // Close statement
+        $stmt->close();
         }
         
-        // Close statement
-        $stmt->close();
+        
     }
     
     // Close connection
