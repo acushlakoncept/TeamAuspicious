ddb92e657d2acef4ddcf37269d8332369a95cb63
Uduak Essien <acushla4real@gmail.com>
2019-09-19 19:12:40 +0100

    database added

--- dashboard.php
+++ dashboard.php
@@ -4,7 +4,7 @@ session_start();
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
+    header("location: index.php");
     exit;
 }
 ?>
@@ -17,9 +17,42 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Welcome to Team Auspicios</title>
-        <meta name="description" content="">
+        
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Primary Meta Tags -->
+  <title>Team Auspicious | HNG Web Track</title>
+  <meta name="title"
+    content="Web developers with successful track records of web team from HNG web track comprising of UI/UX Designers, FrontEnd and BackEnd Developers">
+  <meta name="description"
+    content="An NGO where the parents / a proxy can apply for funding (scholarship) for less privileged children. BlessFlame focus on scholarships for the less privileged to attend primary, secondary and university.">
+  <meta name="keywords"
+    content="Team Auspicious, HNG Task, Web Track, Backend, FrontEnd ">
+  <meta name="robots" content="index, follow">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="language" content="English">
+  <meta name="revisit-after" content="1 days">
+  <meta name="author" content="Team Moscow HNG Intenship">
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="http://teamauspicious.acushlakoncept.com/">
+  <meta property="og:title"
+    content="Web developers with successful track records of web team from HNG web track comprising of UI/UX Designers, FrontEnd and BackEnd Developers">
+  <meta property="og:description"
+    content="Web developers with successful track records of web team from HNG web track comprising of UI/UX Designers, FrontEnd and BackEnd Developers">
+  <meta property="og:image"
+    content="https://res.cloudinary.com/acushlakoncepts/image/upload/v1568902589/team_auspicious_lqphoy.png">
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content="http://teamauspicious.acushlakoncept.com/">
+  <meta property="twitter:title"
+    content="Web developers with successful track records of web team from HNG web track comprising of UI/UX Designers, FrontEnd and BackEnd Developers">
+  <meta property="twitter:description"
+    content="Web developers with successful track records of web team from HNG web track comprising of UI/UX Designers, FrontEnd and BackEnd Developers.">
+  <meta property="twitter:image"
+    content="https://res.cloudinary.com/acushlakoncepts/image/upload/v1568902589/team_auspicious_lqphoy.png">
         
         <!-- Add icon library -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
